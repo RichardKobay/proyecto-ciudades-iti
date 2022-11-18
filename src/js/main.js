ceec58a1191c1ac08d7cf3cd075d00d00c772081
Ricardo Soriano <ricardo.roel110@gmail.com>
2022-11-17 19:27:19 -0600

    shows aguascalientes images. Added some css

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,7 @@ window.addEventListener("load", () => {
 
 function getStates() {
     const xhttp = new XMLHttpRequest()
-    xhttp.open("GET", env.host + "/src/api/api.json", true)
+xhttp.open("GET", env.host + "/src/api/api.json", true)
     xhttp.send()
 
     xhttp.onreadystatechange = function () {
@@ -15,6 +15,7 @@ function getStates() {
             for (const state of data) {
                 const select = document.getElementById("statesSelect")                
                 let opt = document.createElement("option")
+                opt.classList.add("opt")
                 let content = document.createTextNode(state.estado)
                 
                 opt.appendChild(content)
@@ -41,6 +42,8 @@ const getCities = () => {
             let selectedOpt = getSelectedOption(statesSelect).index
             let statesCities = data[selectedOpt].municipios
 
+            citiesSel.setAttribute("id", "citiesSelect")
+            citiesSel.classList.add("select")
             citiesdiv.setAttribute("id", "citiesDiv")
             citiesdiv.classList.add("cities-div")
             body.append(citiesdiv)
@@ -53,10 +56,11 @@ const getCities = () => {
             for (const city of statesCities) {
                 // console.log(city)
                 let opt = document.createElement("option")
+                opt.classList.add("opt")
                 let content = document.createTextNode(city)
                 let valueString = data[selectedOpt].key + "_" + city.replaceAll(" ", "_")
 
-                console.log(valueString)
+                // console.log(valueString)
 
                 opt.appendChild(content)
                 opt.setAttribute("value", valueString)
@@ -72,10 +76,45 @@ const getCities = () => {
             getCitiesBtn.setAttribute("disabled", "")
             getImagesButton.classList.add("button")
             getImagesButton.classList.add("cities-button")
+            getImagesButton.setAttribute("id", "getImagesButton")
             getImagesButton.innerHTML = "Dame las imagenes"
             citiesdiv.appendChild(getImagesButton)
+
+            buttonGetImages()
         }
     }
+}
+
+function getImages() {
+    const body = document.querySelector("body")
+    const citiesSelect = document.getElementById("citiesSelect")
+    const button = document.getElementById("getImagesButton")
+    let selectedOpt = getSelectedOption(citiesSelect).value
+    let containerDiv = document.createElement("div")
+    containerDiv.classList.add("container-div")
+    containerDiv.setAttribute("id", "containerDiv")
+    body.append(containerDiv)
+    
+    for (const imgLink of picDic[selectedOpt]) {
+        let imgContainer = document.createElement("div")
+        imgContainer.classList.add("img-container")
+        imgContainer.setAttribute("id", "imgContainer")
+        let img = document.createElement("img")
+        img.setAttribute("src", imgLink)
+        img.classList.add("state-img")
+        imgContainer.appendChild(img)
+        containerDiv.appendChild(imgContainer)
+    }
+
+    citiesSelect.setAttribute("disabled", "")
+    button.setAttribute("disabled", "")
+
+    const resetButton = document.createElement("button")
+    resetButton.innerHTML = "Reset"
+    resetButton.classList.add("button", "reset-button")
+    resetButton.setAttribute("id", "resetButton")
+    resetButton.setAttribute("onClick", "location.reload(true)")
+    body.appendChild(resetButton)
 }
 
 
@@ -89,3 +128,12 @@ const btn = document.getElementById("button")
 btn.addEventListener("click", () => {
     getCities()
 })
+
+function buttonGetImages() {
+    const button = document.getElementById("getImagesButton")
+    button.addEventListener("click", () => {
+        getImages()
+    })
+}
+
+// console.log(picDic["AGS_Aguascalientes"])
